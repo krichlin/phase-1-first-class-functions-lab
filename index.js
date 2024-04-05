e92c15263637696469fee6655a853f9756141fbf
Ken Richlin <krichlin@gmail.com>
2024-04-05 12:08:01 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+
+let drivers = ['Bill', 'Jenny', 'Carol', 'Jojo'];
+
+function returnFirstTwoDrivers(drivers) {
+    let first_two = drivers.slice(0,2);
+    return (first_two);
+}
+
+function returnLastTwoDrivers(drivers) {
+    let last_two = drivers.slice(-2);
+    return (last_two);
+}
+
+let selectingDrivers = [];
+selectingDrivers.push(returnFirstTwoDrivers);
+selectingDrivers.push(returnLastTwoDrivers);
+
+function createFareMultiplier(multiplier) {
+    return (
+        (fare) => fare = fare * multiplier
+    )
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, driversToReturn ) {
+    return driversToReturn(drivers);
+}
+
+
